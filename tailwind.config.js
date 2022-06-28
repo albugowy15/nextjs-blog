eb3c23df1af7b8e2427cfa774e282c3af35a9403
albugowy15 <muhammadbughowy@gmail.com>
2022-06-28 14:58:20 +0700

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	purge: [
+		'./src/components/**/*.js',
+		'./pages/**/*.js'],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [
+		require( 'tailwindcss' ),
+		require( 'precss' ),
+		require( 'autoprefixer' )
+	]
+}
